4428220184795d3e72b4ab5ea2e2e365bb7d0efc
AlejoMejia96 <alejandroquinteromejia@gmail.com>
2023-11-27 01:47:15 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/robofriends/sw.js', { scope: '/robofriends/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/robofriends-practice/sw.js', { scope: '/robofriends-practice/' })})}
