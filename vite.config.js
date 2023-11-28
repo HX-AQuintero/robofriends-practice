cabeb0797debdb448ad686e1306ec0295a5a6cb8
AlejoMejia96 <alejandroquinteromejia@gmail.com>
2023-11-27 23:20:08 -0500

    Updating PWA config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/robofriends-practice/",
+  base: '/robofriends-practice/',
   plugins: [
     react(),
     VitePWA({
@@ -42,9 +42,6 @@ export default defineConfig({
             purpose: 'maskable',
           },
         ],
-      },
-      devOptions: {
-        enabled: true
       },
     }),
   ],
