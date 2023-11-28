a0986e735cd6782c18998c44c84a5f9213318660
AlejoMejia96 <alejandroquinteromejia@gmail.com>
2023-11-27 22:04:33 -0500

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/robofriends-practice/",
   plugins: [
     react(),
     VitePWA({
@@ -45,7 +46,6 @@ export default defineConfig({
       devOptions: {
         enabled: true
       },
-      base: "/robofriends-practice/"
     }),
   ],
 });
